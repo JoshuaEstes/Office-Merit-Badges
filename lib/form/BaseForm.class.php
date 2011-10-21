f0ff22f1047a00ac28ff8f5547352b31b96819ca
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2011-10-21 18:48:14 -0500

    initial commit

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    OfficeMerits
+ * @subpackage form
+ * @author     Joshua Estes 
+ * @version    SVN: $Id$
+ */
+class BaseForm extends sfFormSymfony
+{
+}
